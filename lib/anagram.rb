de3ea61d559652330b45c18d762c8d8aef8e953c
Jaime Montes de Oca <jaime0422@live.com>
2022-07-20 21:23:32 -0700

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
+require 'pry'
+
 # Your code goes here!
+class Anagram
+    def initialize word
+        @word = word
+    end
+
+    def match words
+        word_in_chars = @word.chars.sort
+
+        # After converting each word to chars and sort the chars then filters only the words equals to the anagram
+        result = words.filter {|word| word.chars.sort == word_in_chars}
+
+        # Return an array with only the words that match
+        result
+    end
+end
+
+# TESTS ==========================
+# game = Anagram.new("listen")
+# game.match(["inlets", "banana", "television", "netlis"])
+
+# game = Anagram.new("ba")
+# game.match(["ab", "abc", "cba"])
+
